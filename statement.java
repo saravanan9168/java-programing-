354dfb823c84d5afc40444dd78657a189d1e31ef
saravanan9168 <saravanan9168@mountzion.ac.in>
2025-08-04 12:26:33 +0530

    Create statement.java

---
+++ statement.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int a=20;
+        int b=21;
+        
+        if (a<b){
+            System.out.println("sathishkumar is greater than sathish");
+        }
+        else{
+             System.out.println("sathishkumar is lesser or eaual than sathish");
+        }
+        
+    }
+}
