655cfe02e82e9bb3343c66be61578ef0ee0c50ca
saravanan9168 <saravanan9168@mountzion.ac.in>
2025-08-05 11:41:54 +0530

    Create for loop 1.java

---
+++ for loop 1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner data= new Scanner(System.in);
+        int c;
+        System.out.println("enter the number");
+        c=data.nextInt();
+        for(c=0;c<=10;c++){
+           System.out.println(c);
+        }
+        
+        
+        
+       
+    }
+}
