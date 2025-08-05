6982b0e4664e1c5c4d8155012e18e88796f019a1
saravanan9168 <saravanan9168@mountzion.ac.in>
2025-08-05 12:26:47 +0530

    Create reverse number.java

---
+++ reverse number.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        int c;
+        for(c=10;c>=0;c--){
+          if(c%2==1){
+              System.out.println(c);
+          }
+        }
+    }
+}
