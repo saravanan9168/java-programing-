2fd03ee98f8e5b9fd39d06c256491b2cee777178
saravanan9168 <saravanan9168@mountzion.ac.in>
2025-08-05 11:16:09 +0530

    do while loop.java

---
+++ do while loop.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner data= new Scanner(System.in);
+        int c;
+        System.out.println("enter the number");
+        c=data.nextInt();
+        do{
+           if(c%2==0){
+                System.out.println("even number :"+c);
+                c--;
+            }
+            else{
+                System.out.println("odd number :"+c);
+                c--;
+            } 
+        }
+        
+        while(c>=0);
+        
+        
+       
+    }
+}
