ba8529d072a0deb4a1ba79b7d0bb5c990e03641e
saravanan9168 <saravanan9168@mountzion.ac.in>
2025-08-11 09:59:21 +0530

    Create 3d array1.java

---
+++ 3d array1.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int[][] arr;
+        arr=new int[3][3];
+        arr[0][0]=1;
+        arr[0][1]=2;
+        arr[0][2]=3;
+        arr[1][0]=5;
+        arr[1][1]=6;
+        arr[1][2]=5;
+        arr[2][0]=5;
+        arr[2][1]=5;
+        arr[2][2]=5;
+        System.out.println(arr);
+        
+    }
+}
